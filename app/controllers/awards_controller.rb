9c094699ccaece0de79e76f20bb0af8cd7fb5a7f
Bliss Man <blissman@gmail.com>
2016-02-24 18:58:23 -0500

    add award and pledge actions

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -5,6 +5,12 @@ class AwardsController < ApplicationController
   end
 
   def create
+    @award = Award.new(award_params)
   end
 
+end
+
+private
+def award_params
+  params.require(:award).permit(:title, :description, :min_amount)
 end
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -5,6 +5,18 @@ class PledgesController < ApplicationController
   end
 
   def create
+    @pledge = Pledge.new(pledge_params)
+    # will probably need to refactor for ajax
+    if @pledge.save
+      redirect_to project_path
+    else
+      render :new
+    end
   end
 
+end
+
+private
+def pledge_params
+  params.require(:pledge).permit(:amount)
 end
