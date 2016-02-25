a79e03dc8ea83be19d9d68bf07c0e9bd959543b4
Bliss Man <blissman@gmail.com>
2016-02-24 19:03:49 -0500

    add user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,40 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    # will need to logout before redirecting to root
+    redirect_to root
   end
 
+end
+
+private
+def user_params
+  params.require(:user).permit(:name, :lastname, :email, :password, :password_confirmation)
 end
