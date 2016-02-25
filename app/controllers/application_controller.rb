2674f7b8ed917544d678d649791e18fe556cd413
Bliss Man <blissman@gmail.com>
2016-02-25 16:57:41 -0500

    sorcery added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,3 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
 end
+
+private
+def not_authenticated
+  redirect_to login_path, alert: "Please login first"
+end
